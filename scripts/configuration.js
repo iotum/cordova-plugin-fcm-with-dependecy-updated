6737d3a27da5b8e0ce6f344f0ec843f262d145f7
Rex Chiu <rex@iotum.com>
2022-08-24 17:18:36 -0400

    remove iOS, incompatible due to outdated firebase and needing removing InstanceID

--- scripts/configuration.js
+++ scripts/configuration.js
@@ -2,28 +2,9 @@
 
 var helpers = require('./helpers');
 
-var APP_NAME = helpers.getValueFromXml(
-    'config.xml',
-    'name',
-    'app name was not found on config.xml'
-);
-var IOS_DIR = 'platforms/ios';
 var ANDROID_DIR = 'platforms/android';
 
 exports.PLATFORM = {
-    IOS: {
-        label: 'ios',
-        dir: IOS_DIR,
-        googleServiceDestinations: [
-            IOS_DIR + '/' + APP_NAME + '/Resources/GoogleService-Info.plist',
-            IOS_DIR + '/' + APP_NAME + '/Resources/Resources/GoogleService-Info.plist'
-        ],
-        googleServiceSources: [
-            'GoogleService-Info.plist',
-            IOS_DIR + '/www/GoogleService-Info.plist',
-            'www/GoogleService-Info.plist'
-        ]
-    },
     ANDROID: {
         label: 'android',
         dir: ANDROID_DIR,
